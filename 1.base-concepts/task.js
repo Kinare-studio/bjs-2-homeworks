9071dc66a08a4c6bc07bf11e83656c55058de365
Arefinkina Regina <regi.kolyadina@yandex.ru>
2021-11-21 23:48:49 +0300

    Third commit variable

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -17,34 +17,55 @@ function solveEquation(a, b, c) {
 
 solveEquation(1, 1, 1);
 
-
-
+// 1 Вариант
 function calculateTotalMortgage(percent, contribution, amount, date = new Date()) {
   let S = amount - contribution;
   let P = percent / 100 / 12;
   let currentMonth = new Date().getMonth();
   let currentYear = new Date().getFullYear();
   let diffferenceInYears = date.getFullYear() - currentYear;
-  let n = diffferenceInYears * 12 - currentMonth + date;
+  let n = diffferenceInYears * 12 - currentMonth + date.getMonth();
   let monthlyPayment = S * (P + (P / (((1 + P) ** n) - 1)));
-  let totalAmount = monthlyPayment * n;
-    let parsedPercent = parseInt(percent);
-    let parsedContribution = parseInt(contribution);
-    let parsedAmount = parseInt(amount);
-    let parsedDate = parseInt(date);
-    if (Number.isNaN(parsedPercent)) {
-      return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
-    } else if (Number.isNaN(parsedContribution)) {
-        return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
-    } else if (Number.isNaN(parsedAmount)) {
-        return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-    } else if (Number.isNaN(parsedDate)) {
-        return `Параметр "Срок" содержит неправильное значение "${date}"`;
-    } else {
-      return totalAmount.toFixed(2);
+  let totalAmount = +(monthlyPayment * n).toFixed(2);
+  let parsedPercent = parseInt(percent);
+  let parsedContribution = parseInt(contribution);
+  let parsedAmount = parseInt(amount);
+  if (Number.isNaN(parsedPercent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (Number.isNaN(parsedContribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (Number.isNaN(parsedAmount)) {
+      return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`; 
+  } else {
+    return totalAmount;
+  }
+}
+
+
+//2 Вариант с циклом
+/*function calculateTotalMortgage(percent, contribution, amount, date = new Date()) {
+  let arrArguments = [
+    ["Процентная ставка", arguments[0]],
+    ["Начальный взнос", arguments[1]],
+    ["Общая стоимость", arguments[2]]
+  ];
+  for (let i = 0; i < arrArguments.length; i++) {
+    let parsedArguments = parseInt(arrArguments[i][1]);
+    if (Number.isNaN(parsedArguments)) {
+      return `Параметр "${arrArguments[i][0]}" содержит неправильное значение "${arrArguments[i][1]}"`;
     }
   }
-  
+  let S = amount - contribution;
+  let P = percent / 100 / 12;
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
+  let diffferenceInYears = date.getFullYear() - currentYear;
+  let n = diffferenceInYears * 12 - currentMonth + date.getMonth();
+  let monthlyPayment = S * (P + (P / (((1 + P) ** n) - 1)));
+  let totalAmount = +(monthlyPayment * n).toFixed(2);
+  return totalAmount;
+}*/
 
+  
   
 
