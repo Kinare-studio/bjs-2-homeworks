e176c684873ed1c82c5fb56c99abc9c807a2ad1f
Arefinkina Regina <regi.kolyadina@yandex.ru>
2021-11-27 17:01:26 +0300

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
+//1 Вариант
+/*function compareArrays(arr1, arr2) {
+  let result = JSON.stringify(arr1) === JSON.stringify(arr2);
+  return result;
+}*/
+
+//2 Вариант
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = (arr1.length == arr2.length) && arr1.every(function(element, index) {
+    return element === arr2[index]; 
+});
+  return result;
 }
+
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter((item) => item >= 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+  return resultArr;
 }
+
