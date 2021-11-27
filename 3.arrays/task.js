516f608737045ee7117077aa0684ec0c9bfcc78c
Arefinkina Regina <regi.kolyadina@yandex.ru>
2021-11-27 17:55:36 +0300

    second commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,20 +1,14 @@
 //1 Вариант
 /*function compareArrays(arr1, arr2) {
-  let result = JSON.stringify(arr1) === JSON.stringify(arr2);
-  return result;
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
 }*/
 
 //2 Вариант
 function compareArrays(arr1, arr2) {
-  let result = (arr1.length == arr2.length) && arr1.every(function(element, index) {
-    return element === arr2[index]; 
-});
-  return result;
+  return arr1.length == arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((item) => item >= 0).filter((item) => item % 3 === 0).map((item) => item * 10);
-  return resultArr;
+  return arr.filter((item) => item >= 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 }
-
