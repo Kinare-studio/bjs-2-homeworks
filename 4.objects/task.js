2987d48b2710ea95613f7622b97bcd11af440724
Arefinkina Regina <regi.kolyadina@yandex.ru>
2021-11-28 12:46:16 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+Student.prototype.setSubject = function(subjectName) {
+  return this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.addMark = function(...[mark]) {
+  if (this.marks === undefined) {
+    return this.marks = [mark];
+  } else {
+    return this.marks.push(mark);
+  }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...mark) {
+  if (typeof this.marks === "object") {
+    return this.marks.push(mark);
+  } else {
+    return this.marks = mark;
+  }
+}
+
+Student.prototype.getAverage = function(...marks) {
+  const arr = this.marks;
+  return average = arr.reduce((acc, item, idx) => {
+    if (idx === arr.length - 1) {
+      return (acc + item) / arr.length;
+      } else {
+      return acc + item;
+    }
+  });
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
