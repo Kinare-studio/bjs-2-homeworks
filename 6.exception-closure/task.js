7628fb2c50debc1bc185b8ec81141822f72dec2e
Arefinkina Regina <regi.kolyadina@yandex.ru>
2021-12-02 14:03:41 +0300

    second commit variables

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,23 +1,15 @@
 //Задача 1
 function parseCount(arg) {
     let result = Number.parseInt(arg);
-    if (isNaN(result) === true) {
-      const parseCountError = new Error("Невалидное значение");
-      throw parseCountError;
+    if (isNaN(result)) {
+      throw new Error("Невалидное значение");
     }
     return result;
   }
 
   function validateCount(arg) {
-    let result = Number.parseInt(arg);
-    try {
-      parseCount = function() {
-        if (isNaN(result) === true) {
-          throw new Error("Невалидное значение");
-        }
-        return result;
-      }
-      return parseCount();
+     try {
+      return parseCount(arg);
     } catch (error) {
       return error;
     }
@@ -30,17 +22,15 @@ function parseCount(arg) {
       this.a = a;
       this.b = b;
       this.c = c;
-      if ((this.a + this.b) < this.c || (this.a + this.c) < this.b || (this.c + this.b) < this.a) {
+      if ((a + b) < c || (a + c) < b || (c + b) < a) {
         throw new Error("Треугольник с такими сторонами не существует");
       }
     }
     getPerimeter() {
-      let p = this.a + this.b + this.c;
-      return p;
+      return this.a + this.b + this.c;
     }
     getArea() {
-      let s = (0.25 * Math.sqrt(4 * Math.pow(this.a, 2) * Math.pow(this.b, 2) - (Math.pow(this.a, 2) + Math.pow(this.b, 2) - Math.pow(this.c, 2)) ** 2)).toFixed(3);
-      return +s;
+      return +((0.25 * Math.sqrt(4 * Math.pow(this.a, 2) * Math.pow(this.b, 2) - Math.pow((Math.pow(this.a, 2) + Math.pow(this.b, 2) - Math.pow(this.c, 2)), 2))).toFixed(3));
     }
   }
   
