463feaf19d5e78648657e4fda1a6be28b6c4a74b
Arefinkina Regina <regi.kolyadina@yandex.ru>
2021-12-30 19:03:41 +0300

    first commit variables

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,32 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+let cache = [];
+    
+  return function wrapper(...args) {
+    let hash = args.join(','); 
+    let idx = cache.findIndex((item) => item.hash === hash); 
+    if (idx !== -1 ) {
+        console.log(`Из кэша: ${cache[idx].result}`);
+        return `Из кэша: ${cache[idx].result}`;
+    }
+    let result = func(...args);
+    cache.push({hash, result});
+    if (cache.length > 5) { 
+      cache.shift(); 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
 }
+
 
 
 function debounceDecoratorNew(func) {
   // Ваш код
 }
 
+
+
 function debounceDecorator2(func) {
   // Ваш код
 }
+
