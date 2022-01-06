40758d85fab521a2aaf6b0bd297d055f2100e392
Arefinkina Regina <regi.kolyadina@yandex.ru>
2022-01-06 15:27:32 +0300

    second commit variables

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,32 +1,49 @@
 function cachingDecoratorNew(func) {
-let cache = [];
-    
+  let cache = [];
+
   return function wrapper(...args) {
-    let hash = args.join(','); 
-    let idx = cache.findIndex((item) => item.hash === hash); 
-    if (idx !== -1 ) {
-        console.log(`Из кэша: ${cache[idx].result}`);
-        return `Из кэша: ${cache[idx].result}`;
+    let hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === hash);
+    if (idx !== -1) {
+      console.log(`Из кэша: ${cache[idx].result}`);
+      return `Из кэша: ${cache[idx].result}`;
     }
     let result = func(...args);
-    cache.push({hash, result});
-    if (cache.length > 5) { 
-      cache.shift(); 
+    cache.push({ hash, result });
+    if (cache.length > 5) {
+      cache.shift();
     }
     console.log("Вычисляем: " + result);
-    return "Вычисляем: " + result;  
+    return "Вычисляем: " + result;
   }
 }
 
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let flag = false;
+
+  return function wrapper(...args) {
+    if (flag === false) {
+      func.apply(this, args);
+    }
+    flag = true;
+    setTimeout(() => flag = false, ms);
+  };
 }
 
 
+function debounceDecorator2(func, ms) {
+  let flag = false;
 
-function debounceDecorator2(func) {
-  // Ваш код
+  function wrapper(...args) {
+    wrapper.count.push(args);
+    if (flag === false) {
+      func.apply(this, args);
+    }
+    flag = true;
+    setTimeout(() => flag = false, ms);
+  }
+  wrapper.count = [];
+  return wrapper;
 }
-
